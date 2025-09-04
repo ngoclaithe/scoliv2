cce0c8ce7ed3aa4933be16b925cf4fd1d07530f8
laingoc99 <laingoc99@gmail.com>
2025-09-04 12:30:18 +0700

    update ne

--- src/pages/PosterPreviewPage.jsx
+++ src/pages/PosterPreviewPage.jsx
@@ -253,7 +253,7 @@ const PosterPreviewPage = () => {
       </div>
 
       {/* Poster Content */}
-      <div className="max-w-4xl mx-auto p-4">
+      <div className="max-w-10xl mx-auto p-4">
         <div className="bg-white rounded-lg shadow-lg overflow-hidden" ref={posterRef}>
           {renderPosterComponent()}
         </div>
